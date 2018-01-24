5e5eaabacd7ee3b44e5f4b12031d2b43320c6512
Afwan Wali Hamim <afwanwalihamim@gmail.com>
2018-01-24 16:11:35 +0700

    [FIX] Remove underscore to avoid any error

--- ftp_backup.sh
+++ ftp_backup.sh
@@ -7,15 +7,15 @@ password="TYPE_FTP_USER_PASSWORD"
 
 cd location/to/odoo/filestore
 
-tar -zcvf BACKUP_$YMD.tar.gz ODOO_DATABASE_NAME/
+tar -zcvf BACKUP$YMD.tar.gz ODOO_DATABASE_NAME/
 
 ftp -n <<EOF
  verbose
  open $IP_address
  user $username $password
- put BACKUP_$YMD.tar.gz
+ put BACKUP$YMD.tar.gz
  bye
 EOF
 
-rm BACKUP_$YMD.tar.gz
+rm BACKUP$YMD.tar.gz
 echo "Sent with love to Backup Server"
